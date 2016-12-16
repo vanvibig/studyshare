a4eca3ca68cf849a0f5fcaaea10c08c43fcdf919
vanvibig <vanvibig@gmail.com>
2016-12-16 16:59:37 +0700

    cuoi bai 3

---
+++ src/ducky/servlets/Authentication.java
@@ -0,0 +1,20 @@
+package ducky.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by KV on 16/12/2016.
+ */
+public class Authentication extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+}
