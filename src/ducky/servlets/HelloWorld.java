f069bfef3eccaa83ac725e8051b8bd827dd2b75c
vanvibig <vanvibig@gmail.com>
2016-12-16 15:41:52 +0700

    cuoi bai 3

--- src/ducky/servlets/HelloWorld.java
+++ src/ducky/servlets/HelloWorld.java
@@ -10,18 +10,26 @@ import java.io.PrintWriter;
 /**
  * Created by KV on 16/12/2016.
  */
-@WebServlet("/hello")
+@WebServlet("/ducky")
 public class HelloWorld extends HttpServlet {
 
 	public HelloWorld(){
 		super();
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
-
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+		loadParameter(request,response,"by Get");
 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
-		response.getWriter().println("Hello KV");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+		loadParameter(request,response,"by Post");
+	}
+
+	protected void loadParameter(HttpServletRequest request, HttpServletResponse response, String method) throws ServletException, java.io.IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		PrintWriter writer = response.getWriter();
+		writer.println("Hello World" + method + "<br>");
+		writer.print("Username " + username + " has " + password);
 	}
 }
---
+++ web/login.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: KV
+  Date: 16/12/2016
+  Time: 3:07 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+	<title>Title</title>
+</head>
+<body>
+	<form style="text-align: center" action="ducky" method="post">
+		Username: <input type="text" name="username" value="">
+		Password: <input type="password" name="password" value="">
+		<input type="submit" value="Ok">
+	</form>
+</body>
+</html>
